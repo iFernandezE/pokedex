f349b7506db8f18d6e6f5af9b653fef519930546
Ivan Fernandez <ivan.fernandez@jala.university>
2023-02-14 12:52:31 -0400

    Weaknesses added

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState , useEffect} from 'react'
-import {v4 as uuidv4} from 'uuid';
+import { useState, useEffect } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import Weaknesses from './components/Weaknesses';
 
 function App() {
+
+
   const pokeApiDomain = `https://pokeapi.co/api/v2/pokemon/`;
   const [currentId, setCurrentId] = useState(1);
-  const [pokemon, setPokemon] = useState({sprites:{}, weight:0, abilities:[]});
-  // const [isLoading, setIsLoading] = useState(false);
+  const [pokemon, setPokemon] = useState({ sprites: {}, weight: 0, abilities: [], types: [] });
+  //const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() =>{
+  useEffect(() => {
     //setIsLoading(true);
     fetch(`${pokeApiDomain}${currentId}`)
-    .then(response => response.json())
-    .then(pokemonData => {
-      console.log(pokemonData);
-      setCurrentId(pokemonData.id);
-      setPokemon(pokemonData);
-      //setIsLoading(false);
-    })
-  },[currentId]);
+      .then(response => response.json())
+      .then(pokemonData => {
+        console.log(pokemonData);
+        setCurrentId(pokemonData.id);
+        setPokemon(pokemonData);
+        //setIsLoading(false);
+      })
+  }, [currentId]);
 
-  const getPokemon = (id)=>{
+  const getPokemon = (id) => {
     setCurrentId(id);
   };
-  
+
   return (
     <div className="App">
       <header className="App-header">
@@ -32,16 +35,20 @@ function App() {
         <div>
           <label>{pokemon.name}</label>
         </div>
-       
+
         <div>
           <img src={pokemon.sprites.front_default} className="App-logo" alt="logo" />
+          {/* <img src={pokemon.sprites.other["official-artwork"].front_default} className="App-logo" alt="logo" /> */}
+
         </div>
-        
+
+        <br></br>
         <div>
           <label>Weight</label>
           <label>{pokemon.weight}</label>
         </div>
-        
+
+        <br></br>
         <div>
           <label>Abilities</label>
           <label>
@@ -52,12 +59,19 @@ function App() {
             ))}
           </label>
         </div>
-        
+
+        <br></br>
         <div>
-          <button onClick={() => getPokemon(currentId -1 )}>Previous</button>
-          <button onClick={() => getPokemon(currentId +1 )}>Next</button>
+          <label>Weaknesses</label>
+          <Weaknesses pokeTypes = {pokemon.types}></Weaknesses>
         </div>
 
+        <br></br>
+        <div>
+          <button onClick={() => getPokemon(currentId - 1)}>Previous</button>
+          <button onClick={() => getPokemon(currentId + 1)}>Next</button>
+        </div>
+        <br></br>
       </header>
     </div>
   );
---
+++ src/components/Weaknesses.js
@@ -0,0 +1,48 @@
+import React from "react";
+import {v4 as uuidv4} from 'uuid';
+
+function Weaknesses(props){
+
+    //fuente: https://www.wikihow.com/Learn-Type-Weaknesses-in-Pok%C3%A9mon 
+    const weaknessesList = [
+        {forType: "bug", weakness: ["fire","rock","flying"]},
+        {forType: "dark", weakness: ["fairy","fighting","bug"]},
+        {forType: "dragon", weakness: ["dragon","ice","fairy"]},
+        {forType: "electric", weakness: ["ground"]},
+        {forType: "fairy", weakness: ["steel","poison"]},
+        {forType: "fighting", weakness: ["fairy","psychic","flying"]},
+        {forType: "fire", weakness: ["ground","rock","water"]},
+        {forType: "flying", weakness: ["electric","rock","ice"]},
+        {forType: "ghost", weakness: ["dark","ghost"]},
+        {forType: "grass", weakness: ["flying","bug","poison","ice","fire"]},
+        {forType: "ground", weakness: ["grass","ice","water"]},
+        {forType: "ice", weakness: ["fire","fighting","rock","steel"]},
+        {forType: "normal", weakness: ["fighting"]},
+        {forType: "poison", weakness: ["ground","psychic"]},
+        {forType: "psychic", weakness: ["bug","dark","ghost"]},
+        {forType: "rock", weakness: ["grass","water","fighting","ground","steel"]},
+        {forType: "steel", weakness: ["ground","fire","fighting"]},
+        {forType: "water", weakness: ["electric","grass"]}
+    ];
+
+    let actualPokWeaknesses = [];
+    props.pokeTypes.forEach(pokeType => {
+        weaknessesList.forEach(types =>{
+            if(pokeType.type.name === types.forType){
+                actualPokWeaknesses.push(...types.weakness)
+            }
+        })
+    });
+
+    return(
+        <div>
+            {actualPokWeaknesses.map(item => (
+              <div key={uuidv4()}>
+                <label>{item}</label>
+              </div>
+            ))}
+        </div>
+    );
+}
+
+export default Weaknesses;
