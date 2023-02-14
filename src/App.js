3a99d90f7b0c82526149612123b47a9750fa0f68
Ivan Fernandez <ivan.fernandez@jala.university>
2023-02-13 20:56:25 -0400

    Project added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState , useEffect} from 'react'
+import {v4 as uuidv4} from 'uuid';
 
 function App() {
+  const pokeApiDomain = `https://pokeapi.co/api/v2/pokemon/`;
+  const [currentId, setCurrentId] = useState(1);
+  const [pokemon, setPokemon] = useState({sprites:{}, weight:0, abilities:[]});
+  // const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() =>{
+    //setIsLoading(true);
+    fetch(`${pokeApiDomain}${currentId}`)
+    .then(response => response.json())
+    .then(pokemonData => {
+      console.log(pokemonData);
+      setCurrentId(pokemonData.id);
+      setPokemon(pokemonData);
+      //setIsLoading(false);
+    })
+  },[currentId]);
+
+  const getPokemon = (id)=>{
+    setCurrentId(id);
+  };
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <div>
+          <label>{pokemon.name}</label>
+        </div>
+       
+        <div>
+          <img src={pokemon.sprites.front_default} className="App-logo" alt="logo" />
+        </div>
+        
+        <div>
+          <label>Weight</label>
+          <label>{pokemon.weight}</label>
+        </div>
+        
+        <div>
+          <label>Abilities</label>
+          <label>
+            {pokemon.abilities.map(item => (
+              <div key={uuidv4()}>
+                <label>{item.ability.name}</label>
+              </div>
+            ))}
+          </label>
+        </div>
+        
+        <div>
+          <button onClick={() => getPokemon(currentId -1 )}>Previous</button>
+          <button onClick={() => getPokemon(currentId +1 )}>Next</button>
+        </div>
+
       </header>
     </div>
   );
